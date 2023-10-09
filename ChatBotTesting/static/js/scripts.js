3c232c3d5fc7b93b15a0d7ec543db615a2137744
isaac21muscat <isaac.muscat.21@um.edu.mt>
2023-10-09 17:29:59 +0200

    Added Comments
    Added Comments

--- ChatBotTesting/static/js/scripts.js
+++ ChatBotTesting/static/js/scripts.js
@@ -63,78 +63,126 @@ var directionsService;
 var directionsRenderer;
 var infoPanel;
 
+//Function to show the user's position on the map
 function showPosition() {
+    //Checking if the browser supports HTML5 geolocation
     if (navigator.geolocation) {
+        //Getting current position and calling the initMap() function with the latitude and longtitude as parameters
         navigator.geolocation.getCurrentPosition(function (position) {
             initMap(position.coords.latitude, position.coords.longitude);
-            console.log(position.coords.latitude, position.coords.longitude);
         });
     } else {
         alert("Sorry, your browser does not support HTML5 geolocation.");
     }
 }
 
+//Function to initialise the map
 function initMap(userLatitude, userLongitude) {
+    //Creating a new Google Map centered at the user's coordinates
     var map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: userLatitude, lng: userLongitude },
         zoom: 15
     });
 
+    //Initialising map-related variables
     infoWindow = new google.maps.InfoWindow();
     directionsService = new google.maps.DirectionsService();
     directionsRenderer = new google.maps.DirectionsRenderer();
 
+    //Setting the map for directions rendering
     directionsRenderer.setMap(map);
 
+    //Getting the element for the information panel
+
+    //Performing a text search for pharmacies using the Places API
     infoPanel = document.getElementById('info-panel');
     var request = {
         query: 'pharmacy',
         fields: ['name', 'geometry'],
     };
-
     var service = new google.maps.places.PlacesService(map);
 
+    //Handling the results of the text search
     service.textSearch(request, function (results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
-            // Sort places by distance
+            //Sorting places by distance from the user's location
             results.sort(function (a, b) {
                 var distanceA = google.maps.geometry.spherical.computeDistanceBetween({ lat: userLatitude, lng: userLongitude }, a.geometry.location);
                 var distanceB = google.maps.geometry.spherical.computeDistanceBetween({ lat: userLatitude, lng: userLongitude }, b.geometry.location);
                 return distanceA - distanceB;
             });
+            //Creating marker for each pharmacy
             for (var i = 0; i < results.length; i++) {
                 createMarker(results[i]);
             }
         }
     });
 
+    //Function to create a marker for a place (pharmacy)
     function createMarker(place) {
+        //Creating a marker
         var marker = new google.maps.Marker({
             map: map,
             position: place.geometry.location,
             title: place.name
         });
-
+        //Adding a click listener to the marker
         marker.addListener("click", function () {
+            //Calling the calculateAndDisplayRoute() function to display the route from the user to the pharmacy
             calculateAndDisplayRoute({ lat: userLatitude, lng: userLongitude }, place.geometry.location);
+            //Creating content for info window
             var content = `<strong>${place.name}</strong><br>${place.formatted_address}<br>`;
             content += `<a href="https://www.google.com/maps?q=${place.geometry.location.lat()},${place.geometry.location.lng()}" target="_blank">Open in Google Maps</a>`;
+            //Setting the content and open the info window
             infoWindow.setContent(content);
             infoWindow.open(marker.getMap(), marker);
         });
     }
 }
 
+//Function to calculate and display the route from origin to destination
 function calculateAndDisplayRoute(origin, destination) {
+    //Creating the route
     directionsService.route({
         origin: origin,
         destination: destination,
         travelMode: 'DRIVING'
     }, function (response, status) {
         if (status === 'OK') {
+            //Setting the directions on the map
             directionsRenderer.setDirections(response);
         } else {
+            //Displaying an alert if directions request fails
             window.alert('Directions request failed due to ' + status);
         }
     });
+}
+
+//Function to communicate with the Chatbot
+function sendMessage() {
+    const userInput = document.getElementById("user-input").value;
+    const chatbox = document.getElementById("chat-box");
+
+    //Displaying the user's message in the chatbox
+    chatbox.innerHTML += `<p>User: ${userInput}</p>`;
+
+    //Making an API call to GPT-3 using JavaScript's fetch() function
+    fetch('/api/chat', {
+        method: 'POST',
+        body: JSON.stringify({ text: userInput }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            //Displaying the response from GPT-3 in the chatbox
+            chatbox.innerHTML += `<p>Bot: ${data.text}</p>`;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+    //Clearing the user input field
+    document.getElementById("user-input").value = '';
 }
